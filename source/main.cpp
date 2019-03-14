cb0c1856787f9cca383b4ef05ed63e83982cee71
DongWook Lee <vztpv@naver.com>
2019-03-14 21:14:33 +0900

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -5,6 +5,8 @@
 
 #include "WrapPointer.h"
 
+using namespace wiz;
+
 class Node;
 class Item {
 public:
@@ -19,14 +21,22 @@ public:
 	}
 };
 
+class Node2
+{
+public:
+	WrapPointer<Node2> next;
+};
+
 class Node {
 public:
 	int val;
 	WrapPointer<Node> parent;
 	WrapPointer<Node> child;
-	WrapPointer<Node> next;
+	WrapPointer<Node2> next;
 public:
 	Node() {
+		next = WrapPointer<Node2>::New();
+
 		parent.SetName("parent");
 		child.SetName("child");
 		next.SetName("next");
@@ -39,19 +49,31 @@ public:
 	}
 };
 
+void memory_leak_test() {
+	//
+}
+void double_delete_test() {
+	//
+}
+void array_test() {
+	//
+}
+
 
 int main(void)
 {
-	FileManager::fileName = "output.txt";
+	FileManager::fileName = "output.txt"; 
 	FileManager::ClearFile();
 
 	// Test
 	WrapPointer<Node> x = WrapPointer<Node>::NewWithName("x", 3);
 	WrapPointer<Node> y = WrapPointer<Node>::NewWithName("y", 5);
-	WrapPointer<WrapPointer<Node>> z = WrapPointer<WrapPointer<Node>>::NewArray("z", 10);
 
-	x->next = y;
-	y->next = x;
+	WrapPointer<Node> z = WrapPointer<Node>::NewArray("z", 10);
+	//WrapPointer<WrapPointer<Node>> z = WrapPointer<WrapPointer<Node>>::NewArray("z", 10);
+
+	x->parent = y;
+	y->parent = x;
 
 	x.Delete();
 	y.Delete();
@@ -60,16 +82,16 @@ int main(void)
 	x = WrapPointer<Node>::NewWithName("a", 3); 
 	y = WrapPointer<Node>::NewWithName("b", 5);
 
-	x->next = y;
-	y->next = x;
-	z[0] = y + 1;
+	x->parent = y;
+	y->parent = x;
+	z[0].parent = y + 1;
 	
 	//y.Delete();
 	
-	z[1] = z[0] - 1;
-
+	z[1].parent = z[0].parent - 1;
+	//z[10] = z[1];
 	x.Delete();
-	//
+	z[1].parent.Delete();
 	z.DeleteArray();
 
 	return 0;
